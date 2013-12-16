5c32cbb3a5b938d679668789450d8b83f9c537cd
James Tomasino <james@tomasino.org>
2013-12-16 16:02:39 -0500

    added a couple basic functions

--- sjs/libs.js
+++ sjs/libs.js
@@ -1,1 +1,2 @@
 //= require ./lib/jquery-1.10.1.min
+//= require lib/jquery.numeric
--- sjs/main.js
+++ sjs/main.js
@@ -1,13 +1,28 @@
 //= require ELO
 
+var num_items = 10;
+
+$('#num_items').numeric({ decimal: false, negative: false });
+
 // How many people do you need to rate?
+function selectNumItems( ) {
+	num_items = parseInt(document.getElementById('num_items').value, 10);
+}
+
 // Name them.
+function nameItem () {
+
+}
+
 // Randomize List
 // Create pair mapping
-// Prompt one at a time
-// Sort list
-// Display
 
+// Prompt one at a time
+// Calculate
 function calculate ( result ) {
 
 }
+
+// Sort list
+// Display
+
