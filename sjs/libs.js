444694da308cf0d98ee51f3d00e0eecf7965afc5
James Tomasino <james@tomasino.org>
2013-12-16 15:56:42 -0500

    added basic html elements for interface

--- sjs/libs.js
+++ sjs/libs.js
@@ -1,2 +1,1 @@
 //= require ./lib/jquery-1.10.1.min
-//= require lib/jquery.numeric
--- sjs/main.js
+++ sjs/main.js
@@ -1,36 +1,13 @@
 //= require ELO
 
-var white = new ELO();
-var black = new ELO();
+// How many people do you need to rate?
+// Name them.
+// Randomize List
+// Create pair mapping
+// Prompt one at a time
+// Sort list
+// Display
 
-$('input[type=text]').numeric({ decimal: false, negative: false });
-$('input[type=text]').focus(function () {
-	$(this).val('');
-	$(this).next().html('');
-});
+function calculate ( result ) {
 
-
-function calculate() {
-
-	var winner = document.getElementsByName('winner');
-	for (var i = 0; i < winner.length; ++i ){
-		if (winner[i].checked) {
-			result = (winner[i].value == "black") ? 0 : (winner[i].value == "white") ? 1 : 0.5;
-			break;
-		}
-	}
-
-	rating_1 = parseInt(document.getElementById('player_1_rating').value, 10);
-	rating_2 = parseInt(document.getElementById('player_2_rating').value, 10);
-
-	white.rating = rating_1;
-	black.rating = rating_2;
-
-	white.play(black, result);
-
-	document.getElementById('player_1_new_rating').innerHTML = String(white.rating);
-	document.getElementById('player_2_new_rating').innerHTML = String(black.rating);
-
-	$('.output').show();
 }
-
